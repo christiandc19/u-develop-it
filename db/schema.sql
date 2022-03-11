1c411906ee3410c7abd717f1208491bb520def6b
unknown <christiandc19@hotmail.com>
2022-03-10 23:28:04 -0800

    created voters table

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
 CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -13,6 +14,15 @@ CREATE TABLE candidates (
     last_name VARCHAR(30) NOT NULL,
     party_id INTEGER,
     industry_connected BOOLEAN NOT NULL,
-    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL /* constraint. This allows us to flag the party_id field as an official foreign key and tells SQL which table and field it references. 
-    We added ON DELETE SET NULL to tell SQL to set a candidate's party_id field to NULL if the corresponding row in parties is ever deleted. */
+    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL /* constraint. This allows us to flag the party_id field as an official foreign key and tells SQL 
+    which table and field it references. We added ON DELETE SET NULL to tell SQL to set a candidate's party_id field to NULL if the corresponding row in parties is ever deleted. */
+);
+
+CREATE TABLE voters (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP /* In SQL, a DATETIME field's value will look something like 2020-01-01 13:00:00. That probably doesn't seem 
+    very user-friendly, but the front-end team can take that value, convert it with JavaScript's Date() constructor, and display anything they want at that point. */
 );

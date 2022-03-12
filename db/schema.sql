5bc848bc22475c6d72379a67720661d01b6625c7
unknown <christiandc19@hotmail.com>
2022-03-12 04:00:18 -0800

    created a votes table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS votes;
 
 CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -25,4 +26,15 @@ CREATE TABLE voters (
     email VARCHAR(50) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP /* In SQL, a DATETIME field's value will look something like 2020-01-01 13:00:00. That probably doesn't seem 
     very user-friendly, but the front-end team can take that value, convert it with JavaScript's Date() constructor, and display anything they want at that point. */
+);
+
+CREATE TABLE votes (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    voter_id INTEGER NOT NULL,
+    candidate_id INTEGER NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT uc_voter UNIQUE (voter_id), /* uc_voter, signifies that the values inserted into the voter_id field must be unique. For example, whoever has a voter_id of 1 can only appear in this table once. */
+    CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE, /* Previously, ON DELETE SET NULL would set the record's field to NULL if the key from the reference table was deleted. 
+    With ON DELETE CASCADE, deleting the reference key will also delete the entire row from this table. */
+    CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,8 @@ INSERT INTO parties (name, description)
 VALUES
     ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
     ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
-    ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
+    ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.'),
+    ('Avengers', 'Earths mightiest super hero');
 
 
 INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
@@ -16,7 +17,7 @@ VALUES
     ('Edward', 'Bellamy', 3, 0),
     ('Montague', 'Summers', 3, 1),
     ('Octavia', 'Butler', 3, 1),
-    ('Unica', 'Zurn', NULL, 1);
+    ('Unica', 'Zurn', 4, 1);
     
 
 INSERT INTO voters (first_name, last_name, email)
